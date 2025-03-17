675dc9f450a3b0bc1559d5c530135ca732c73cee
majd <amhb371@gmail.com>
2025-03-17 05:18:59 +0330

    add profile form completed

--- src/components/account/Profile-form.tsx
+++ src/components/account/Profile-form.tsx
@@ -6,15 +6,16 @@ import {
   Box,
   Typography,
   useMediaQuery,
+  Modal,
 } from "@mui/material";
 import moment from "moment-jalaali";
 import { AdapterMomentJalaali } from "@mui/x-date-pickers/AdapterMomentJalaali";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Avatar, IconButton } from "@mui/material";
-import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import addImage from "assets/gallery-add.png";
 
-import { CustomButton } from "uiKit";
+import { CustomButton, EditIcons } from "uiKit";
 import theme from "theme";
 
 const ProfilePictureUploader: React.FC = () => {
@@ -42,8 +43,11 @@ const ProfilePictureUploader: React.FC = () => {
           alignItems: "center",
         }}
       >
-        <Avatar
-          src={image || "/default-profile.jpg"}
+        <Box
+          component={"img"}
+          borderRadius={"50%"}
+          bgcolor={theme.palette.grey[300]}
+          src={image || ""}
           sx={{ width: 80, height: 80, border: "2px dashed #ddd" }}
         />
         {/* Upload Button Overlay */}
@@ -51,17 +55,16 @@ const ProfilePictureUploader: React.FC = () => {
           component="label"
           sx={{
             position: "absolute",
-            bottom: 5,
-            right: 5,
-            background: "rgba(255, 255, 255, 0.8)",
+            bottom: 30,
+            right: 30,
             borderRadius: "50%",
             width: 24,
             height: 24,
             boxShadow: 1,
-            "&:hover": { background: "rgba(255, 255, 255, 1)" },
+            "&:hover": { background: "transparent" },
           }}
         >
-          <AddAPhotoIcon sx={{ fontSize: 16, color: "#555" }} />
+          <Box component={"img"} src={addImage} />
           <input
             type="file"
             hidden
@@ -93,6 +96,7 @@ interface FormData {
 
 export const ProfileForm: React.FC = () => {
   const isMobile = useMediaQuery("(max-width:768px)");
+  const [open, setOpen] = useState(false);
 
   const {
     control,
@@ -111,6 +115,7 @@ export const ProfileForm: React.FC = () => {
 
   const onSubmit = (data: FormData) => {
     console.log("Form Data:", data);
+    setOpen(true);
   };
 
   return (
@@ -295,12 +300,22 @@ export const ProfileForm: React.FC = () => {
           </LocalizationProvider>
         </Box>
       </Box>
-      <Box display={"flex"} justifyContent={"space-between"}>
-        <Box display={"flex"} gap={"8px"} alignItems={"center"}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        flexDirection={isMobile ? "column" : "row"}
+        gap={"12px"}
+      >
+        <Box
+          display={"flex"}
+          gap={"8px"}
+          alignItems={"center"}
+          justifyContent={isMobile ? "space-between" : "flex-start"}
+        >
           <Typography fontSize={"12px"}>وضعیت اتصال به ربات تلگرام</Typography>
           <CustomButton
             variant="contained"
-            sx={{ backgroundColor: "#4DB2D2", fontWeight: 500 }}
+            sx={{ backgroundColor: "#4DB2D2", fontWeight: 500, height: "34px" }}
           >
             اتصال به تلگــــــــــــرام
           </CustomButton>
@@ -310,7 +325,7 @@ export const ProfileForm: React.FC = () => {
           type="submit"
           variant="contained"
           sx={{
-            width: "222px",
+            width: isMobile ? "100%" : "222px",
             height: "34px",
             fontSize: "16px",
             fontWeight: 500,
@@ -320,6 +335,76 @@ export const ProfileForm: React.FC = () => {
           ذخیره تغییــــــــــرات{" "}
         </Button>
       </Box>
+      {/* Modal Component */}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 342,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: "25px 31px",
+            borderRadius: "15px",
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            gap: "16px",
+          }}
+        >
+          <Box display={"flex"} width={"100%"} justifyContent={"center"}>
+            <Box
+              display={"flex"}
+              alignItems={"center"}
+              width={50}
+              height={50}
+              bgcolor={theme.palette.grey[300]}
+              borderRadius={"50%"}
+              justifyContent={"center"}
+            >
+              <EditIcons width={20} height={20} />
+            </Box>
+          </Box>
+          <Typography fontSize={"14px"} fontWeight={700}>
+            آیا اطلاعات وارد شده را تایـیـــــــــــــــد میکنید..؟
+          </Typography>
+          <Typography fontSize={12} color="gray" textAlign={"justify"}>
+            چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است،
+            و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود
+            ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و
+            آینده، شناخت فراوان جامعه و متخصصان را می طلبد.{" "}
+          </Typography>
+          <Box display={"flex"} gap={"12px"} width={"100%"}>
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{
+                width: "100%",
+                height: 34,
+                p: "4px",
+                color: theme.palette.primary[600],
+              }}
+              onClick={() => setOpen(false)}
+            >
+              انصراف
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                width: "100%",
+                height: 34,
+                p: "4px",
+                bgcolor: theme.palette.primary[600],
+              }}
+              onClick={() => setOpen(false)}
+            >
+              اطلاعات را تایید میکنم
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </Box>
   );
 };
