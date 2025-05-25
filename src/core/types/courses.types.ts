cf1e95d80d4081e74a6d4b9182980bc10c276fee
majd <amhb371@gmail.com>
2025-05-26 02:08:23 +0330

    remove console

--- src/core/types/courses.types.ts
+++ src/core/types/courses.types.ts
@@ -65,10 +65,8 @@ export interface CoursesFeedbackDataTypes {
   summary: Partial<Record<FeedbackCategory, FeedbackItem>>;
 }
 
-// تعریف سطوح
 export type Level = "0" | "1" | "2" | "3" | "4" | "5" | "6";
 
-// ساختار داده‌ی یک دوره‌ی آکادمی
 export interface CourseLevelAcademyItem {
   title: string;
   uuid: string;
