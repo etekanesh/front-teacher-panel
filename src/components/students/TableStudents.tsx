85d6e8ad860e5054c233be6ff4d5d299f77f5f99
majd <amhb371@gmail.com>
2025-06-09 07:33:15 +0330

    remove console

--- src/components/students/TableStudents.tsx
+++ src/components/students/TableStudents.tsx
@@ -43,7 +43,6 @@ export const TableStudents: React.FC<Props> = ({ handleOpen }) => {
     [studentsListData, paginationModel.page, paginationModel.pageSize]
   );
 
-  console.log("totalObjects :>> ", totalObjects);
   const columns: GridColDef[] = [
     {
       field: "fullName",
