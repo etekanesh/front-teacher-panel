6cab80459375520304e720d03946ab32b9608c48
Mazimi <mohammad.azimi.1996@gmail.com>
2025-08-17 18:31:56 +0330

    enable menus

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -102,8 +102,8 @@ const SidebarMenu = [
   {
     title: "فروش و مارکتینگ",
     icon: (color: any) => <MarketingIcons color={color} />,
-    // link: "/teacher/marketingt",
-    link: "/",
+    link: "/teacher/marketingt",
+    // link: "/",
     child: [
       {
         title: "مدیریت فروش و درآمد",
@@ -122,8 +122,8 @@ const SidebarMenu = [
   {
     title: "مدیریت دوره ها",
     icon: (color: any) => <TaskIcons color={color} />,
-    // link: "/teacher/courses",
-    link: "/",
+    link: "/teacher/courses",
+    // link: "/",
   },
   {
     title: "مدیریت دانشجویان",
@@ -150,8 +150,8 @@ const SidebarMenu = [
   {
     title: "پیــــــــام ها",
     icon: (color: any) => <MessagesIcons color={color} />,
-    // link: "/teacher/messages",
-    link: "/",
+    link: "/teacher/messages",
+    // link: "/",
   },
   {
     title: "فــــــــــروم ",
