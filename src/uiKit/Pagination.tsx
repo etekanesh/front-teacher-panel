0ca798b0e45e2395f6b2b397467faa0690a429a0
sir-shahin <shahin.n14001995@gmail.com>
2026-02-24 18:09:45 +0330

    feat: pagination responsive with dynamic sibling

--- src/uiKit/Pagination.tsx
+++ src/uiKit/Pagination.tsx
@@ -2,52 +2,52 @@
 import React from "react";
 import Pagination from "@mui/material/Pagination";
 import {
-    useGridApiContext,
-    useGridSelector,
-    gridPageSelector,
-    gridPageCountSelector,
+  useGridApiContext,
+  useGridSelector,
+  gridPageSelector,
+  gridPageCountSelector,
 } from "@mui/x-data-grid";
-import { PaginationItem } from "@mui/material";
+import { PaginationItem, useMediaQuery } from "@mui/material";
 import WestIcon from "@mui/icons-material/West";
 import EastIcon from "@mui/icons-material/East";
 
 export const CustomPagination: React.FC = () => {
-    const apiRef = useGridApiContext();
-    const page = useGridSelector(apiRef, gridPageSelector);
-    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+  const apiRef = useGridApiContext();
+  const page = useGridSelector(apiRef, gridPageSelector);
+  const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+  const isMobile = useMediaQuery("(max-width:768px)");
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    console.log("event :>> ", event);
+    apiRef.current.setPage(value - 1);
+  };
 
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        console.log("event :>> ", event);
-        apiRef.current.setPage(value - 1);
-    };
-
-    return (
-        <Pagination
-            count={pageCount}
-            page={page + 1}
-            onChange={handleChange}
-            color="primary"
-            sx={{ mt: 2, display: "flex", justifyContent: "center" }}
-            siblingCount={2}
-            boundaryCount={0}
-            renderItem={(item) => (
-                <PaginationItem
-                    slots={{ previous: WestIcon, next: EastIcon }}
-                    {...item}
-                    sx={{
-                        borderRadius: "50%",
-                        width: 40,
-                        height: 40,
-                        fontSize: "16px",
-                        fontWeight: "bold",
-                        bgcolor: item.selected ? "success.main" : "#f0f0f0",
-                        color: item.selected ? "white" : "#333",
-                        "&:hover": {
-                            bgcolor: item.selected ? "success.dark" : "#e0e0e0",
-                        },
-                    }}
-                />
-            )}
+  return (
+    <Pagination
+      count={pageCount}
+      page={page + 1}
+      onChange={handleChange}
+      color="primary"
+      sx={{ mt: 2, display: "flex", justifyContent: "center" }}
+      siblingCount={isMobile ? 1 : 2}
+      boundaryCount={0}
+      renderItem={(item) => (
+        <PaginationItem
+          slots={{ previous: WestIcon, next: EastIcon }}
+          {...item}
+          sx={{
+            borderRadius: "50%",
+            width: 40,
+            height: 40,
+            fontSize: "16px",
+            fontWeight: "bold",
+            bgcolor: item.selected ? "success.main" : "#f0f0f0",
+            color: item.selected ? "white" : "#333",
+            "&:hover": {
+              bgcolor: item.selected ? "success.dark" : "#e0e0e0",
+            },
+          }}
         />
-    );
+      )}
+    />
+  );
 };
